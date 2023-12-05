7a43aa684bf9e8f6d72e2144616e029db9e6bda5
= <josefergo89@gmail.com>
2023-12-04 21:27:47 -0500

    api acabada

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 import Consultas from "../Consultas.js"
 import Router from "express"
+//biblioteca para manejar la carga de archivos en formularios
 import multer from "multer"
 import path from 'path'
-
 import crypto from 'node:crypto'
 
 const route = Router()
@@ -46,17 +46,19 @@ route.post('/iniciar-sesion', async (req, res) => {
         let usuarioAutenticado = "false";
         let perfil = ''
         let nombre = ''
+        let correou = ''
 
         result.forEach(resul => {
             if (correo === resul.correo && contra === resul.contraseña) {
                 usuarioAutenticado = "true";
                 perfil = resul.foto_perfil
                 nombre = resul.nombre
+                correou = resul.correo
             }
         });
 
         if (usuarioAutenticado == "true") {
-            res.json({ nombre: nombre, validacion: usuarioAutenticado, foto: perfil });
+            res.json({ nombre: nombre, validacion: usuarioAutenticado, foto: perfil, correo: correou });
         } else {
             res.json({ message: 'Credenciales inválidas' });
         }
@@ -91,18 +93,18 @@ route.post('/crear', upload.single('foto'), async (req, res) => {
 
 
 route.delete('/eliminar/:id', async (req, res) => {
-    // try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const deletee = await consulta.delete('publicaciones', `id = '${id}'`)
-    console.log(deletee)
+        const deletee = await consulta.delete('publicaciones', `id = '${id}'`)
+        console.log(deletee)
 
-    res.send("bien")
+        res.send("bien")
 
-    // } catch (error) {
-    //     console.log("error" + error)
-    //     res.send("mal" + error)
-    // }
+    } catch (error) {
+        console.log("error" + error)
+        res.send("mal" + error)
+    }
 })
 
 route.get('/tabla', async (req, res) => {
@@ -111,6 +113,37 @@ route.get('/tabla', async (req, res) => {
         res.json(result)
         await consulta.closeConect();
     } catch (error) {
+        res.json({ error: "hubo un error" + error })
+        await consulta.closeConect();
+    }
+})
+
+route.patch('/modificar/:id', upload.single('foto'), async (req, res) => {
+    try {
+        const { id } = req.params
+        const { titulo } = req.body
+
+        let result
+        let foto_publicacion
+
+        if (req.file && req.file.filename) {
+            foto_publicacion = req.file.filename;
+        }
+
+        const ruta_foto = `/uploads/${foto_publicacion}` // ruta que va guardar en la db
+
+        if (titulo && foto_publicacion) {
+            result = await consulta.update('publicaciones', `imagen = '${ruta_foto}', titulo = '${titulo}'`, `id ='${id}'`)
+        } else if (foto_publicacion) {
+            result = await consulta.update('publicaciones', `imagen = '${ruta_foto}'`, `id ='${id}'`)
+        } else if (titulo) {
+            result = await consulta.update('publicaciones', `titulo = '${titulo}'`, `id ='${id}'`)
+        }
+
+        console.log(result)
+        res.json(result)
+    } catch (error) {
+        console.log(error)
         res.json({ error: "hubo un error" + error })
         await consulta.closeConect();
     }
